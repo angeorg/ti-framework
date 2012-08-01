339147d461230b841f24feb2addf469a0bda8995
Adrian Dimitrov <dimitrov.adrian@gmail.com>
2012-08-01 18:48:40 +0300

    * Removed class Pagination. * Added new function Pagination (remaped legacy class pagination). * Added new hooks paginate and paginate_

--- ti-framework.php
+++ ti-framework.php
@@ -3091,8 +3091,8 @@ function num_to_month($num = 0, $long_names = FALSE) {
  */
 function sql_now($timestamp = 0) {
   return !$timestamp
-    ? date( 'Y-m-d H:i:s' )
-    : date( 'Y-m-d H:i:s', $timestamp );
+  ? date( 'Y-m-d H:i:s' )
+  : date( 'Y-m-d H:i:s', $timestamp );
 }
 
 /**
@@ -3246,7 +3246,7 @@ function transliterate($string = '', $from_latin = FALSE) {
 if ( !class_exists( 'Application') ):
 /**
  * Main Application class, provide a flexible MVC alike separation.
- */
+*/
 class Application {
 
   static private $is_main = TRUE;
@@ -3439,7 +3439,7 @@ endif;
 if ( !class_exists( 'Database') ):
 /**
  * Database wraper class for PDO
- */
+*/
 class Database extends PDO {
 
   /**
@@ -3709,7 +3709,7 @@ endif;
 if ( !class_exists( 'Messagebus') ):
 /**
  * Messagebus class.
- */
+*/
 class Messagebus {
 
   /**
@@ -3789,7 +3789,7 @@ endif;
 if ( !class_exists( 'Calendar') ):
 /**
  * Calendar based events.
- */
+*/
 class Calendar {
 
   /**
@@ -4027,7 +4027,7 @@ endif;
 if ( !class_exists( 'Pagination') ):
 /**
  * Simple class for work with images.
- */
+*/
 class Image {
 
   /**
@@ -4514,193 +4514,130 @@ class Image {
 }
 endif;
 
-if ( !class_exists( 'Pagination') ):
 /**
- * Simple pagination class
+ * Build HTML paginate links for page.
+ *
+ * <?php
+ *   // Example simple ussage:
+ *   echo paginate( $page, $entries->getTotal(), 'articles/page/%s' );
+ * ?>
+ *
+ * @fire paginate
+ * @fire paginate_$id
+ *
+ * @param int $page_no
+ *   current page number
+ * @param int $entries
+ *   total number of entries
+ * @param string $base_url
+ *   url to be used
+ * @param bool $echo
+ *   return or echo the generated html
+ * @param string $id
+ *   used only when need to customize,
+ *   specific paginate section by this id and hook paginate
+ *
+ * @return null|string
  */
-class Pagination {
+function paginate($page_no, $entries, $base_url = '', $echo = TRUE, $id = '') {
 
-  /**
-   * Current page
-   */
-  public $current_page = 1;
+  $conf = new stdClass;
+  $conf->size = 10;
+  $conf->per_page = 20;
+  $conf->html_cell_normal = '<a href="%s">%s</a>';
+  $conf->html_cell_active = '<a href="%s" class="active">%s</a>';
+  $conf->html_cell_first = '<a href="%s">&#8592;</a>';
+  $conf->html_cell_last = '<a href="%s">&#8594;</a>';
+  $conf->html_wrapper = '<div class="pagination">%s</div>';
 
-  /**
-   * Total num of records to paginate
-   */
-  public $total_rows = 100;
+  $hook_name = 'paginate' . ( $id ? '_' . $id : '' );
+  $conf = do_hook( $hook_name, $conf );
 
-  /**
-   * How many per page to show
-   */
-  public $per_page = 20;
-
-  /**
-   * Base url, printf format
-   */
-  public $base_url = '';
-
-  /**
-   * How many cells to show
-   */
-  public $size = 10;
-
-  /**
-   * Format of normal cell (printf format)
-   */
-  public $html_cell_normal = '<a href="%s">%s</a>';
-
-  /**
-   * Format of active cell (printf format)
-   */
-  public $html_cell_active = '<a href="%s" class="active">%s</a>';
-
-  /**
-   * Format of first cell (printf format)
-   */
-  public $html_cell_first = '<a href="%s">&#8592;</a>';
-
-  /**
-   * Format of last cell (printf format)
-   */
-  public $html_cell_last = '<a href="%s">&#8594;</a>';
-
-  /**
-   * Format of wrapper (printf format)
-   */
-  public $html_wrapper = '<div class="pagination">%s</div>';
-
-  private $html = '';
-
-  /**
-   * Constructor, if array or object is passed, then initialize the object with vals.
-   *
-   * @param mixed
-   *
-   * @return Pagination
-   */
-  function __construct($config = array()) {
-
-    $config = do_hook( 'pagination_config', $config );
-
-    if ( $config ) {
-      foreach ( CAST_TO_ARRAY( $config ) as $key => $val ) {
-        if ( isset($this->{$key}) ) {
-          $this->{$key} = $val;
-        }
-      }
-    }
-    return $this;
+  if ( $conf->per_page >= $entries ) {
+    return FALSE;
   }
 
-  /**
-   * Generate the paginator
-   *
-   * @return Pagination
-   */
-  public function generate() {
+  $html = '';
 
-    if ( $this->per_page >= $this->total_rows ) {
-      return FALSE;
-    }
+  $conf->per_page = CAST_TO_INT( $conf->per_page, 1 );
 
-    $html = '';
+  $page_num_last = ceil( $entries / $conf->per_page );
 
-    $this->per_page = CAST_TO_INT($this->per_page, 1);
+  if ( $page_no > $page_num_last ) {
+    $page_no = $page_num_last;
+  }
+  elseif ( $page_no < 1 ) {
+    $page_no = 1;
+  }
 
-    $page_num_last = ceil( $this->total_rows / $this->per_page );
+  $page_num_prev = $page_no > 1 ? $page_no - 1 : 1;
+  $page_num_next = $page_no < $page_num_last ? $page_no + 1 : $page_num_last;
 
-    if ( $this->current_page > $page_num_last ) {
-      $this->current_page = $page_num_last;
-    }
-    elseif ($this->current_page < 1) {
-      $this->current_page = 1;
-    }
+  if ( $conf->size ) {
+    $half_size = floor( $conf->size / 2 );
 
-    $page_num_prev = $this->current_page > 1 ? $this->current_page - 1 : 1;
-    $page_num_next = $this->current_page < $page_num_last ? $this->current_page + 1 : $page_num_last;
+    $even = $this->size % 2 ? 1 : 0;
 
-    if ( $this->size ) {
-      $half_size = floor($this->size / 2);
+    $for_loops = $page_no + $half_size + $even;
+    $i = $page_no - $half_size + 1;
 
-      $even = $this->size % 2 ? 1 : 0;
-
-      $for_loops = $this->current_page + $half_size + $even;
-      $i = $this->current_page - $half_size + 1;
-
-      if ($this->current_page - $half_size < 1) {
-        $for_loops = $this->size;
-        $i = 1;
-      }
-
-      if ($for_loops > $page_num_last) {
-        $for_loops = $page_num_last;
-        $i = $page_num_last - $this->size + 1;
-      }
-
-      if ($i < 1) {
-        $i = 1;
-      }
-    }
-    else {
-      $for_loops = $page_num_last;
+    if ( $page_no - $half_size < 1 ) {
+      $for_loops = $conf->size;
       $i = 1;
     }
 
-    if ( $this->current_page > 1 ) {
-      if ( $this->html_cell_first ) {
-        $html .= sprintf( $this->html_cell_first, site_url( sprintf( $this->base_url, 1 ) ) );
-      }
+    if ( $for_loops > $page_num_last ) {
+      $for_loops = $page_num_last;
+      $i = $page_num_last - $conf->size + 1;
     }
 
-    for ($s = 1; $i <= $for_loops; $i++, $s++) {
-      $uri = site_url( sprintf( $this->base_url, $i ) );
-
-      if ($this->current_page == $i) {
-        $html .= sprintf( $this->html_cell_active, $uri, $i );
-      }
-      else {
-        $html .= sprintf( $this->html_cell_normal, $uri, $i );
-      }
+    if ($i < 1) {
+      $i = 1;
     }
-
-    if ( $page_num_last > $this->current_page ) {
-      if ( $this->html_cell_last ) {
-        $html .= sprintf( $this->html_cell_last, site_url( sprintf( $this->base_url, $page_num_last ) ) );
-      }
-    }
-
-    if ( !$html ) {
-      return '';
-    }
-
-    if ( $this->html_wrapper ) {
-      $html = sprintf( $this->html_wrapper, $html );
-    }
-
-    $this->html = $html;
-
-    return $this;
+  }
+  else {
+    $for_loops = $page_num_last;
+    $i = 1;
   }
 
-  /**
-   * Output the generated calendar html.
-   */
-  public function show() {
-    echo $this->html;
+  if ( $page_no > 1 ) {
+    if ( $conf->html_cell_first ) {
+      $html .= sprintf( $conf->html_cell_first, site_url( sprintf( $base_url, 1 ) ) );
+    }
   }
 
-  /**
-   * Return the generated calendar html.
-   *
-   * @return string
-   */
-  public function get_html() {
-    return $this->html;
+  for ( $s = 1; $i <= $for_loops; $i++, $s++ ) {
+    $uri = site_url( sprintf( $base_url, $i ) );
+
+    if ( $page_no == $i) {
+      $html .= sprintf( $conf->html_cell_active, $uri, $i );
+    }
+    else {
+      $html .= sprintf( $conf->html_cell_normal, $uri, $i );
+    }
   }
 
+  if ( $page_num_last > $page_no ) {
+    if ( $conf->html_cell_last ) {
+      $html .= sprintf( $conf->html_cell_last, site_url( sprintf( $base_url, $page_num_last ) ) );
+    }
+  }
+
+  if ( !$html ) {
+    return '';
+  }
+
+  if ( $conf->html_wrapper ) {
+    $html = sprintf( $conf->html_wrapper, $html );
+  }
+
+  if ( $echo ) {
+    echo $html;
+  }
+  else {
+    return $html;
+  }
 }
-endif;
 
 /**
  * @} End of "defgroup framework classes".
