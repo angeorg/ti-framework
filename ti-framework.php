33ae9fcb47e4b18ead9e80d2359ffa05dd4b171d
Adrian Dimitrov <dimitrov.adrian@gmail.com>
2012-08-28 12:31:12 +0300

    * Fixes on num_to_month(). * Added num_to_weekday(). * Removed class Calendar. * Added function calendar().

--- ti-framework.php
+++ ti-framework.php
@@ -2584,7 +2584,7 @@ function paginate($page_no, $entries, $per_page = 20, $base_url = '', $echo = TR
     return '';
   }
 
-  if ( $conf->html_wrapper ) {
+  if ( strpos( $conf->html_wrapper, '%s' ) !== FALSE ) {
     $html = sprintf( $conf->html_wrapper, $html );
   }
 
@@ -2594,6 +2594,265 @@ function paginate($page_no, $entries, $per_page = 20, $base_url = '', $echo = TR
   else {
     return $html;
   }
+}
+
+/**
+ * Generate the calendar's html
+ *
+ * @access public
+ *
+ * @param date $current_date
+ *
+ * @return Calendar
+ */
+function calendar($current_date = '', $base_url = 'archive/%Y-%m-%d', $link_all_days = TRUE, $events = array(), $echo = TRUE, $id = '') {
+
+  $conf = new stdClass;
+  $conf->id = $id;
+  $conf->first_is_monday = FALSE;
+  $conf->show_weekday_names = TRUE;
+
+  $conf->html_wrap = '<table class="table-calendar">%calendar</table>';
+  $conf->html_row = '<tr>%cells</tr>';
+
+  $conf->html_row_weekdays = '<tr>%cells</tr>';
+  $conf->html_cell_weekday = '<th class="%class">%name</th>';
+
+  $conf->html_cell_other= '<th class="%class">%name</th>';
+
+  $conf->html_cell_empty = '<td class="%class"><a href="%url">%day</a>';
+  $conf->html_cell_events = '<td class="%class"><a href="%url" title="' . __( 'There is %s events on this date' ) . '">%day</a>';
+
+  $conf->events &= $events;
+
+  $hook_name = 'calendar' . ( $id ? '-' . $id : '' );
+  $conf->html_wrapper = '<table id="' . $hook_name .  '" class="calendar">%s</div>';
+  $conf = do_hook( 'calendar', $conf );
+  $conf = do_hook( $hook_name, $conf );
+
+  $html = '';
+
+  list($y, $m, $d) = sscanf($current_date, '%d-%d-%d');
+  if ( !$y ) {
+    gmdate( 'Y' );
+  }
+  if ( !$m ) {
+    gmdate( 'm' );
+  }
+  if ( !$d ) {
+    gmdate( 'd' );
+  }
+
+  $html = '';
+
+  $html_row = '';
+  if ( $conf->show_weekday_names) {
+    $timestamp = strtotime('next ' . ($conf->first_is_monday ? 'Monday' : 'Sunday') );
+    $days = array();
+    for ($i = 0; $i < 7; $i++) {
+      $html_row .= strtr( $conf->html_cell_weekday, array(
+        '%name' => __( strftime( '%a', $timestamp ) ),
+        '%class' => 'weekday-' . $i,
+      ));
+      $timestamp = strtotime('+1 day', $timestamp);
+    }
+    $html .= strtr( $conf->html_row_weekdays, array(
+      '%cells' => $html_row,
+    ));
+  }
+
+  $month_first_time = gmmktime( 0, 0, 0, $m, 1, $y );
+  $month_days = gmdate('t', $month_first_time );
+  $day = 1;
+  $w = (int) gmdate( 'w', $month_first_time );
+  $day = gmdate('t', gmmktime( 0, 0, 0, $m, -1, $y ) ) - $w+1;
+  $html_row = '';
+  for ( $weekday = 0; $weekday < $w; $weekday++, $day++ ) {
+    $html_row .= sprintf( $conf->html_cell_other, $day );
+  }
+  unset($w);
+  for ( $day = 1; $day <= $month_days; $day++ ) {
+
+    $classes = array( 'day-' . $day, 'weekday-' . $weekday );
+    $html_row .= strtr( $conf->html_cell_empty_normal, array(
+      '%day' => $day,
+      '%url' => site_url( strftime( $base_url, strtotime( $y . '-' . $m . '-' . $d ) ) ),
+      '%class' => implode( ' ', $classes ),
+    ));
+
+    if ($weekday >= 6) {
+      $weekday = 0;
+      $html .= sprintf( $conf->html_row, $html_row );
+      $html_row = '';
+    }
+    else {
+      $weekday++;
+    }
+  }
+  if ( $weekday ) {
+    for ( $day = 1; $weekday <= 6; $weekday++, $day++ ) {
+      $html_row .= sprintf( $conf->html_cell_other, $day );
+    }
+    $html .= $html_row;
+  }
+
+  if ( !$html ) {
+    return '';
+  }
+
+  $html = str_replace( '%cells', $html );
+
+  if ( $echo ) {
+    echo $html;
+  }
+  else {
+    return $html;
+  }
+
+
+exit;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $current_date = explode('-', $current_date);
+
+  if (!$current_date[0]) {
+    $current_date[0] = date('Y');
+  }
+
+  if (!isset($current_date[1])) {
+    $current_date[1] = date('m');
+  }
+
+  if (!isset($current_date[2])) {
+    $current_date[2] = date('d');
+  }
+
+  $Y = str_pad( CAST_TO_INT( $current_date[0] ), 4, 0, STR_PAD_LEFT );
+  $M = str_pad( CAST_TO_INT( $current_date[1], 1, 12 ), 2, 0, STR_PAD_LEFT );
+  $D = str_pad( CAST_TO_INT( $current_date[2], 1, 31 ), 2, 0, STR_PAD_LEFT );
+
+  $day_weekday = date( 'w', mktime( 0, 0, 0, $M, 1, $Y ) );
+  if ( $day_weekday === 0 ) {
+    $day_weekday = 7;
+  }
+
+  $html_row = '';
+  if ( $conf->show_weekday_names) {
+    $timestamp = strtotime('next ' . ($conf->first_is_monday ? 'Monday' : 'Sunday') );
+    $days = array();
+    for ($i = 0; $i < 7; $i++) {
+      $html_row .= '<th>' . strftime('%a', $timestamp) . '</th>';
+      $timestamp = strtotime('+1 day', $timestamp);
+    }
+    $html .= sprintf( $conf->html_row, $html_row );
+  }
+
+  for ($i = 1; $i < ($conf->first_is_monday ? $day_weekday : $day_weekday + 1); $i++) {
+    #$html .= '<td class="hidden">&nbsp;</td>';
+  }
+
+  $month_days = date('t', mktime(0, 0, 0, $M, 1, $Y));
+
+  $html_row = '';
+  for ($i = 1; $i <= $month_days; $i++) {
+
+    $date = $Y . '-' . $M . '-' . $i;
+    $link = site_url( strftime( $base_url, strtotime( $date ) ) );
+
+    if ( isset( $conf->events_dates[$date] ) ) {
+      $events = count( $conf->events_dates[$date] );
+    }
+    else {
+      $events = 0;
+    }
+
+    $title = $events ? sprintf( __( 'Events on this date - %d.' ), $events ) : __( 'There is no events on this date' );
+
+    if ( $link_all_days ) {
+      $html_row .= '<td><a href="' . $link . '" title="' . $title . '">' . $i . '</a></td>';
+    }
+    elseif ( $events ) {
+      $html_row .='<td><a href="' . $link . '" title="' . $title . '">' . $i . '</a></td>';
+    }
+    else {
+      $html_row .='<td>' . $i . '</td>';
+    }
+
+    if ( $conf->first_is_monday ) {
+      if ( $day_weekday === 7 ) {
+        $html .= sprintf( $conf->html_row, $html_row );
+        $html_row = '';
+        $day_weekday = 1;
+        continue;
+      }
+    }
+    else {
+      if ( $day_weekday === 7 ) {
+        $day_weekday = 1;
+        continue;
+      }
+      elseif ( $day_weekday === 6 ) {
+        $html .= sprintf( $conf->html_row, $html_row );
+        $html_row = '';
+      }
+    }
+    $day_weekday++;
+  }
+
+  echo $html;
+  return;
+
+  for (; $day_weekday <= ( $conf->first_is_monday ? 7 : 6 ); $day_weekday++ ) {
+    $html .= '<td>&nbsp;</td>';
+  }
+
+  $html .= '</tr>';
+
+
 }
 
 /**
@@ -3224,13 +3483,34 @@ function dec_to_roman($decimal = 0) {
  * @return string
  */
 function num_to_month($num = 0, $long_names = FALSE) {
-  $months = $long_names
-  ? array(1 => 'january', 2 => 'february', 3 => 'march', 4 => 'april', 5 => 'may', 6 => 'june',
-      7 => 'july', 8 => 'august', 9 => 'september', 10 => 'october', 11 => 'november', 12 => 'december')
-      : array(1 => 'jan', 2 => 'feb', 3 => 'mar', 4 => 'apr', 5 => 'may', 6 => 'jun', 7 => 'jul', 8 => 'aug',
-          9 => 'sep', 10 => 'oct', 11 => 'nov', 12 => 'dec');
-  $num = abs( CAST_TO_INT($num) );
-  return isset( $months[$num] ) ? __( $months[$num] ) : '';
+
+  if ( $long_names ) {
+    $months = array( 'January', 'February', 'March', 'April', 'May', 'June', 'July',
+                      'August', 'September', 'October', 'November', 'December' );
+  }
+  else {
+    $months = array( 'Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.', 'Jul.',
+                      'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.' );
+  }
+  return isset( $months[$num+1] ) ? $months[$num+1] : NULL;
+}
+
+/**
+ * Number to weekday.
+ *
+ * @param int $num
+ * @param bool $long_names
+ *
+ * @return string
+ */
+function num_to_weekday($num = 0, $long_names = FALSE) {
+  if ( $long_names ) {
+    $days = array( 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' );
+  }
+  else {
+    $days = array( 'Sun.', 'Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.' );
+  }
+  return isset( $days[$num+1] ) ? $days[$num+1] : NULL;
 }
 
 /**
@@ -3822,244 +4102,6 @@ class TI_Messagebus {
 
 }
 
-/**
- * Calendar based events.
- */
-if ( !class_exists( 'Calendar') ):
-class Calendar {
-
-  /**
-   * Link template (spritnf suitable string)
-   *
-   * @var $link_template
-   *
-   * @access public
-   */
-  public $link_template = 'blog/%Y-%m-%d';
-
-  /**
-   * Link all days, instead of these with events only
-   *
-   * @var $link_all_days
-   *
-   * @access public
-   */
-  public $link_all_days = TRUE;
-
-  /**
-   * First day of week is monday.
-   *
-   * @var $first_is_monday
-   *
-   * @access public
-   */
-  public $first_is_monday = TRUE;
-
-  /**
-   * Show weekdays names, instead of numbers.
-   *
-   * @var $show_weekday_names
-   *
-   * @access public
-   */
-  public $show_weekday_names = TRUE;
-
-  /**
-   * Markup to wrap the table (printf suitable string)
-   *
-   * @access public
-   *
-   * @var $html_table_open
-   */
-  public $html_table_wrap = '<table class="table-calendar">%s</table>';
-
-  private $events_dates = array();
-  private $html = '';
-
-  /**
-   * Constructor, if array or object is passed, then initialize the object with vals.
-   *
-   * @param mixed
-   *
-   * @return Calendar
-   */
-  function __construct($config = array()) {
-
-    $config = do_hook( 'calendar_config', $config );
-
-    if ( $config ) {
-      foreach ( CAST_TO_ARRAY( $config ) as $key => $val ) {
-        if ( isset($this->{$key}) ) {
-          $this->{$key} = $val;
-        }
-      }
-    }
-    return $this;
-  }
-
-  /**
-   * Add event to the calendar.
-   *
-   * @access public
-   *
-   * @param string $date
-   *
-   * @return boolean
-   */
-  public function add_event($date = '', $text = '') {
-
-    if (is_array($date) || is_object($date)) {
-      foreach ( $date as $key => $val ) {
-        $this->add_event( $key, $val );
-      }
-    }
-    else {
-      $date = CAST_TO_STRING( $date );
-    }
-
-    if ( !$date ) {
-      return FALSE;
-    }
-
-    if ( !isset($this->events_dates[$date]) ) {
-      $this->events_dates[$date] = array();
-    }
-
-    $this->events_dates[$date][] = $text;
-
-    return TRUE;
-  }
-
-  /**
-   * Generate the calendar's html
-   *
-   * @access public
-   *
-   * @param date $YMD
-   *
-   * @return Calendar
-   */
-  public function generate($YMD = '') {
-
-    $this->html = '';
-
-    $YMD = explode('-', $YMD);
-
-    if (!$YMD[0]) {
-      $YMD[0] = date('Y');
-    }
-
-    if (!isset($YMD[1])) {
-      $YMD[1] = date('m');
-    }
-
-    if (!isset($YMD[2])) {
-      $YMD[2] = date('d');
-    }
-
-    $Y = str_pad( CAST_TO_INT( $YMD[0] ), 4, 0, STR_PAD_LEFT );
-    $M = str_pad( CAST_TO_INT( $YMD[1], 1, 12 ), 2, 0, STR_PAD_LEFT );
-    $D = str_pad( CAST_TO_INT( $YMD[2], 1, 31 ), 2, 0, STR_PAD_LEFT );
-
-    $day_weekday = date( 'w', mktime( 0, 0, 0, $M, 1, $Y ) );
-    if ( $day_weekday === 0 ) {
-      $day_weekday = 7;
-    }
-
-    $this->html .= '<tr>';
-
-    if ( $this->show_weekday_names) {
-      for ( $i = 0; $i < 7; $i++ ) {
-        $this->html .= '<th>' . $i . '</th>';
-      }
-      $this->html .= '</tr><tr>';
-    }
-
-    for ($i = 1; $i < ($this->first_is_monday ? $day_weekday : $day_weekday + 1); $i++) {
-      $this->html .= '<td class="hidden">&nbsp;</td>';
-    }
-
-    $month_days = date('t', mktime(0, 0, 0, $M, 1, $Y));
-
-    for ($i = 1; $i <= $month_days; $i++) {
-
-      $date = $Y . '-' . $M . '-' . $i;
-      $link = site_url(strftime($this->link_template, strtotime($date)));
-
-      if (isset($this->events_dates[$date])) {
-        $events = count($this->events_dates[$date]);
-      }
-      else {
-        $events = 0;
-      }
-
-      $title = $events ? sprintf(__('Events on this date - %d.'), $events) : __('There is no events on this date');
-
-      if ($this->link_all_days) {
-        $this->html .= '<td><a href="' . $link . '" title="' . $title . '">' . $i . '</a></td>';
-      }
-      elseif ($events) {
-        $this->html .='<td><a href="' . $link . '" title="' . $title . '">' . $i . '</a></td>';
-      }
-      else {
-        $this->html .='<td>' . $i . '</td>';
-      }
-
-      if ($this->first_is_monday) {
-        if ($day_weekday === 7) {
-          $this->html .= "</tr><tr>";
-          $day_weekday = 1;
-          continue;
-        }
-      }
-      else {
-        if ($day_weekday === 7) {
-          $day_weekday = 1;
-          continue;
-        }
-        elseif ($day_weekday === 6) {
-          $this->html .= '</tr><tr>';
-        }
-      }
-      $day_weekday++;
-    }
-
-    for (; $day_weekday <= ($this->first_is_monday ? 7 : 6); $day_weekday++) {
-      $this->html .= '<td>&nbsp;</td>';
-    }
-
-    $this->html .= '</tr>';
-
-    if ( strpos( $this->html_table_wrap, '%s' ) !== FALSE ) {
-      $this->html = sprintf( $this->html_table_wrap, $this->html );
-    }
-
-    return $this;
-  }
-
-  /**
-   * Output the generated calendar html.
-   *
-   * @access public
-   */
-  public function show() {
-    echo $this->html;
-  }
-
-  /**
-   * Return the generated calendar html.
-   *
-   * @access public
-   *
-   * @return string
-   */
-  public function get_html() {
-    return $this->html;
-  }
-
-}
-endif;
-
 if ( !class_exists( 'Image') ):
 /**
  * Simple class for work with images.
@@ -4642,6 +4684,7 @@ if ( defined('TI_DOCUMENTATION') && is_string( TI_DOCUMENTATION )
   exit;
 }
 
+// If we defined TI_DISABLE_BOOT, then using the framework only as PHP library.
 if ( !defined( 'TI_DISABLE_BOOT' )) {
 
   // Set debugging mode.
